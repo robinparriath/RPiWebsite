2f935cbf033fcaa489ca7dfd99b17ca4390038ea
Robin S. Parriath <robinparriath@gmail.com>
2020-08-14 13:05:54 +0530

    Fixed github_to_server script to pull before copy

--- github_to_server.sh
+++ github_to_server.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 source_folder="/media/HDD/git/RPiWebsite/"
 target_folder="/media/HDD/"
+
+cd $source_folder
+git pull
+
 for file in `cat github_to_server.txt`
 do cp $source_folder$file $target_folder$file
 done
