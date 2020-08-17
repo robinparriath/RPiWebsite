ac3a2fb8b1da73a67550d5c4836306b057d060fa
Robin S. Parriath <robinparriath@gmail.com>
2020-08-17 20:00:02 +0530

    Data for 2020-08-17

--- server_to_github.sh
+++ server_to_github.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 source_folder="/media/HDD/"
 target_folder="/media/HDD/git/RPiWebsite/"
+cd $source_folder
 for file in `cat server_to_github.txt`
 do cp $source_folder$file $target_folder$file
 done
