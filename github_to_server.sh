298daa9cd9e671dbe7644af4112cbb4e285ebd77
Robin Parriath <robinparriath@gmail.com>
2020-08-23 16:27:58 +0530

    removed pull. Will be handled by Git hooks

--- github_to_server.sh
+++ github_to_server.sh
@@ -3,7 +3,7 @@ source_folder="/media/HDD/git/RPiWebsite/"
 target_folder="/media/HDD/"
 
 cd $source_folder
-git pull
+#git pull
 
 for file in `cat github_to_server.txt`
 do cp $source_folder$file $target_folder$file
